da91ec433fad075d06fb2cdc5f773c0532e392f3
catskippy <82828461+catskippy@users.noreply.github.com>
2022-05-10 09:23:07 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,51 @@
+<?php
+header('HTTP/1.1 404 Not Found');
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="ru"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="ru"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="ru"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="ru">
+<!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+  <title>Error 404</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="stylesheet" href="css/error.css">
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  <!--[if lt IE 9]>
+    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <![endif]-->
+</head>
+<body>
+  <div class="comingcontainer">
+    <div class="checkbacksoon">
+      <p>
+        <span class="go3d">4</span>
+        <span class="go3d">0</span>
+        <span class="go3d">4</span>
+      </p>
+      <p class="error">
+        The page you were looking for does not exist :( .<br> But dont worry, from time to time, it happens.<br>
+        Most likely, you entered the wrong page address. <br>You can go to the home page of the site and start all over again.</p>
+      <!--		<form action="" method="post" class="search">-->
+      <!--  <input type="search" name="" placeholder="поиск" class="input" />-->
+      <!--  <input type="submit" name="" value="" class="submit" />-->
+      <!--</form>-->
+      <nav>
+        <ul>
+          <li><a href="https://mcalpha205.cf/">Home</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</body>
+</html>
