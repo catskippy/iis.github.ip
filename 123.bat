0e006c2a88c9e839809bd397ac5b202b56bb5309
catskippy <82828461+catskippy@users.noreply.github.com>
2022-11-20 20:21:06 +0300

    Add files via upload

---
+++ 123.bat
@@ -0,0 +1,3 @@
+@Echo off
+Start "Chrome" "%Systemdrive%\Program Files\Google\Chrome\Application\chrome.exe" --incognito" "https://drive.google.com/file/d/17KuWqwYCI3joz0TRGmesWZsd5_SnFxmu/view?usp=share_link"
+Exit /b
