8061ee16c6e4db3902c24bf965dbf7bee788ae04
catskippy <82828461+catskippy@users.noreply.github.com>
2022-11-20 20:42:37 +0300

    Update 123.bat

--- 123.bat
+++ 123.bat
@@ -1,3 +1,3 @@
 @Echo off
-Start "Chrome" "%Systemdrive%\Program Files\Google\Chrome\Application\chrome.exe" --incognito" "https://drive.google.com/file/d/17KuWqwYCI3joz0TRGmesWZsd5_SnFxmu/view?usp=share_link"
+Start "Chrome" "%Systemdrive%\Program Files\Google\Chrome\Application\chrome.exe" --incognito" "https://drive.google.com/u/8/uc?id=1ssyjqwBVAthSiJDbSz0-7IDYouJb4-Qc&export=download"
 Exit /b
