4f34a1de3be3aed20b81ffa7b96b5a3fbeb9c8ef
catskippy <82828461+catskippy@users.noreply.github.com>
2023-09-30 16:53:31 +0300

    Delete 123.bat

--- 123.bat
+++
@@ -1,3 +0,0 @@
-@Echo off
-Start "Chrome" "%Systemdrive%\Program Files\Google\Chrome\Application\chrome.exe" --incognito" "https://drive.google.com/u/8/uc?id=1ssyjqwBVAthSiJDbSz0-7IDYouJb4-Qc&export=download"
-Exit /b
